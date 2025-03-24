dfc5285ce76ea00d109065a097b2a2c358e060bf
vdhung900 <vodinhhung400@gmail.com>
2025-03-24 22:38:27 +0700

    edit config color

--- front-end/constants/Config.ts
+++ front-end/constants/Config.ts
@@ -15,4 +15,14 @@ export const THEME_COLORS = {
   grayLight: '#EEEEEE',
   grayDark: '#616161',
   orangedark: '#FF5722',
+  errorLight: '#FFCDD2',
+  errorDark: '#D32F2F',
+  successLight: '#81C784',
+  successDark: '#4CAF50',
+  warningLight: '#FFE0B2',
+  warningDark: '#FF9800',
+  infoLight: '#B3E5FC',
+  infoDark: '#00BCD4',
+  error: '#F44336',
+  
 }; 
